6f3d3c386090afd9b76544bca0c8e889d4e358c3
Santos Michelena <santos.michelena@hhi.fraunhofer.de>
2023-10-04 16:06:34 +0200

    getting back to it....

--- test/testProblemUtils.jl
+++ test/testProblemUtils.jl
@@ -33,7 +33,7 @@ function test_checkSample()
     @test_throws AssertionError checkSample(p, s2)
 
     # Test case where R dimensions don't match
-    s3 = Sample([rand(2), rand(3)], rand(2,2), rand(9), rand())
+    s3 = Sample([rand(2), rand(3)], rand(2, 2), rand(9), rand())
     @test_throws AssertionError checkSample(p, s3)
 
     # Test case where r dimensions don't match
@@ -41,7 +41,12 @@ function test_checkSample()
     @test_throws AssertionError checkSample(p, s4)
 
     # Test case where sample type is wrong
-    s5 = Sample([rand(Int32, 2), rand(Int32, 3)], rand(Int32, 9, 9), rand(Int32, 9), rand(Int32))
+    s5 = Sample(
+        [rand(Int32, 2), rand(Int32, 3)],
+        rand(Int32, 9, 9),
+        rand(Int32, 9),
+        rand(Int32),
+    )
     @test_throws AssertionError checkSample(p, s5)
 
 end
@@ -52,50 +57,50 @@ function test_checkData()
     # Test case where all samples pass the check
     S1 = [
         Sample([rand(2), rand(3)], rand(9, 9), rand(9), rand()),
-        Sample([rand(2), rand(3)], rand(9, 9), rand(9), rand())
+        Sample([rand(2), rand(3)], rand(9, 9), rand(9), rand()),
     ]
     @test checkData(p, S1) == S1
 
     # Test case where one sample fails the check
     S2 = [
         Sample([rand(2), rand(3)], rand(9, 9), rand(9), rand()),
-        Sample([rand(2), rand(3)], rand(4, 9), rand(9), rand()) #wrong R
+        Sample([rand(2), rand(3)], rand(4, 9), rand(9), rand()), #wrong R
     ]
     @test_throws AssertionError checkData(p, S2)
 end
 
 function test_checkGamma()
     P = QuadTeamProblem(2, [2, 3], [4, 5], Float64)
-    
+
     # Test case where checkGamma should return true
     γ₁¹(x) = [x[1], x[1], x[1], x[1]]
     γ₂¹(x) = [x[1], x[1], x[1], x[1], x[1]]
-   
+
     γ¹ = [γ₁¹, γ₂¹]
 
     @test checkGamma(P, γ¹) == γ¹
 
     # Test case where checkGamma should return false
-    γ₁²(x) = [2*x[1]]
+    γ₁²(x) = [2 * x[1]]
     γ₂²(x) = [x[1]]
-   
+
     γ² = [γ₁², γ₂²]
-    
+
     @test_throws AssertionError checkGamma(P, γ²)
 end
 
 function test_loss()
     P = QuadTeamProblem(2, [2, 3], [4, 5], Float64)
-    
+
     # Test case where loss should return a valid result
     Y = [rand(2), rand(3)]
     R = rand(9, 9)
     r = rand(9)
     c = 1.0
-  
+
     γ₁(x) = [x[1], x[1], x[1], x[1]]
     γ₂(x) = [x[1], x[2], x[3], x[1], x[2]]
-   
+
     γ = [γ₁, γ₂]
 
     sample = Sample(Y, R, r, c)
@@ -106,14 +111,14 @@ end
 function test_risk()
     # Define a sample set
     S = [
-        Sample([rand(2), rand(3)], rand(4,4), rand(4), rand()),
-        Sample([rand(2), rand(3)], rand(4,4), rand(4), rand())
+        Sample([rand(2), rand(3)], rand(4, 4), rand(4), rand()),
+        Sample([rand(2), rand(3)], rand(4, 4), rand(4), rand()),
     ]
 
     # Define a vector of functions
     γ = [
         x -> sum(x), #output dim = 1
-        x -> x .* 2.0 #output dim = dim(Y2) = 3
+        x -> x .* 2.0, #output dim = dim(Y2) = 3
     ]
 
     # Calculate the risk
